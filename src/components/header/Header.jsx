6da3bfb82613cca0dbdf7423123b307d388f664d
Moritz Künzl <mo.kuenzl@gmail.com>
2022-04-15 16:10:48 +0200

    Initial commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import PersonalPicture from '../../assets/me.png'
+import HeaderSocial from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Moritz Künzl</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial/>
+
+        <div className='personalPicture'>
+          <img src={PersonalPicture} alt="Myself" />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
