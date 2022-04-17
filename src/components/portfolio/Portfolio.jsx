41a9a39791a9cfa4c6aa3f27c25b4ea5a3cab50b
Moritz Künzl <mo.kuenzl@gmail.com>
2022-04-17 10:55:01 +0200

    added portfolio component with css and media queries

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './portfolio.css'
+import PortfolioImg1 from '../../assets/portfolio1.jpg'
+import PortfolioImg2 from '../../assets/portfolio2.jpg'
+import PortfolioImg3 from '../../assets/portfolio3.jpg'
+
+const projectData = [
+  {
+    id: 1,
+    image: PortfolioImg1,
+    title: 'Projekt 1',
+    github: 'https://Github.com'
+  },
+  {
+    id: 2,
+    image: PortfolioImg2,
+    title: 'Projekt 2',
+    github: 'https://Github.com'
+  },
+  {
+    id: 3,
+    image: PortfolioImg3,
+    title: 'Projekt 3',
+    github: 'https://Github.com'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My Stuff</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          projectData.map(({id, image, title, github}) => {
+            return (
+              <article className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={image} alt=''/>
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className='btn btn-primary' target='_blank'>Github</a>
+                </div>        
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
