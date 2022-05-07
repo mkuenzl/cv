423dde80beaf1659031f392c5c135bb23ce8e57d
Moritz Künzl <s4mokuen@uni-trier.de>
2022-05-07 14:52:13 +0200

    removed unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -13,11 +11,9 @@ const App = () => {
   return (
     <html>
       <Header />
-      <Nav />
       <About />
       <Experience />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
     </html>
