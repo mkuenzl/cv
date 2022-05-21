bae7b8efdc2325af13a8873df48e08d7007526c0
Moritz Künzl <mo.kuenzl@gmail.com>
2022-05-21 18:22:20 +0200

    added change text on hover - about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 import './about.css'
 import PersonalPicture from '../../assets/me-about.jpg'
 import {GiPlantRoots} from 'react-icons/gi'
@@ -9,6 +10,12 @@ import {BiCodeCurly} from 'react-icons/bi'
 const About = () => {
 
   const [aboutText, setAboutText] = useState("Original Text");
+  const standardText = 'In the last years I focused on my master studies, which I' + 
+  'successfully finished in September 2021. Now I am looking' +
+  'for further challenges and a job where I can learn new things' +
+  'and develop myself.' +
+  'I would describe myself as a calm and open person who' +
+  'learns quickly, listens well and values punctuality.';
 
   return (
     <section id='about'>
@@ -22,35 +29,24 @@ const About = () => {
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
+            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Plants")} onMouseLeave={() => setAboutText(standardText)}>
               <GiPlantRoots className='about__icon'/>
               <h5>Plants</h5>
             </article>
-            <article className='about__card'>
+            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Cooking")} onMouseLeave={() => setAboutText(standardText)}>
               <GiCampCookingPot className='about__icon'/>
               <h5>Cooking</h5>
             </article>
-            <article className='about__card' onPress={() => setAboutText("Changed Text")}>
+            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Bouldering")} onMouseLeave={() => setAboutText(standardText)}>
               <GiMountainClimbing className='about__icon'/>
               <h5>Bouldering</h5>
             </article>
-	        <article className='about__card'>
+	        <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
               <BiCodeCurly className='about__icon'/>
               <h5>Coding</h5>
             </article>
           </div>
           <p>{aboutText}</p>
-          {/*
-          <p>
-              In the last years I focused on my master studies, which I 
-              successfully finished in September 2021. Now I am looking 
-              for further challenges and a job where I can learn new things 
-              and develop myself. 
-              I would describe myself as a calm and open person who 
-              learns quickly, listens well and values punctuality.
-
-          </p>
-          */}
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
