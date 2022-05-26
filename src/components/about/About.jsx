5573fa1a0a4d92f70837d90e4beb2d7b78148a19
Moritz Künzl <mo.kuenzl@gmail.com>
2022-05-26 15:21:03 +0200

    changed layout of about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useState } from 'react';
 import './about.css'
-import PersonalPicture from '../../assets/me-about.jpg'
+import PersonalPicture from '../../assets/pictures/me-about.jpg'
 import {GiPlantRoots} from 'react-icons/gi'
 import {GiCampCookingPot} from 'react-icons/gi'
 import {GiMountainClimbing} from 'react-icons/gi'
 import {BiCodeCurly} from 'react-icons/bi'
+import {SiPcgamingwiki} from 'react-icons/si'
 
 const About = () => {
 
   const [aboutText, setAboutText] = useState("Original Text");
-  const standardText = 'In the last years I focused on my master studies, which I' + 
-  'successfully finished in September 2021. Now I am looking' +
-  'for further challenges and a job where I can learn new things' +
-  'and develop myself.' +
-  'I would describe myself as a calm and open person who' +
+  const standardText = 'In the last years I focused on my master studies, which I ' + 
+  'successfully finished in September 2021. Now I am looking ' +
+  'for further challenges and a job where I can learn new things ' +
+  'and develop myself. ' +
+  'I would describe myself as a calm and open person who ' +
   'learns quickly, listens well and values punctuality.';
 
   return (
@@ -22,11 +23,7 @@ const About = () => {
       <h5>Get To Know</h5>
       <h2>A Little Bit About Me</h2>
       <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={PersonalPicture} alt="About picture of myself." />
-          </div>
-        </div>
+
         <div className="about__content">
           <div className="about__cards">
             <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Plants")} onMouseLeave={() => setAboutText(standardText)}>
@@ -35,19 +32,31 @@ const About = () => {
             </article>
             <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Cooking")} onMouseLeave={() => setAboutText(standardText)}>
               <GiCampCookingPot className='about__icon'/>
-              <h5>Cooking</h5>
+              <h5>Food</h5>
             </article>
             <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Bouldering")} onMouseLeave={() => setAboutText(standardText)}>
               <GiMountainClimbing className='about__icon'/>
-              <h5>Bouldering</h5>
+              <h5>Boulder</h5>
             </article>
-	        <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
+	          <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
               <BiCodeCurly className='about__icon'/>
-              <h5>Coding</h5>
+              <h5>Code</h5>
+            </article>
+            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
+              <SiPcgamingwiki className='about__icon'/>
+              <h5>Games</h5>
             </article>
           </div>
-          <p>{aboutText}</p>
-          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+
+          <div className="about__me">
+            <div className="about__me-image">
+              <img src={PersonalPicture} alt="About picture of myself." />
+            </div>
+            <div className="about__me-text">
+              <p>{aboutText}</p>
+            </div>
+          </div>
+
         </div>
       </div>
     </section>
