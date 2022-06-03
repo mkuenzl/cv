8f3b0f15852aa779110912c38f178529c36bbaf8
Moritz Künzl <mo.kuenzl@gmail.com>
2022-06-03 16:12:30 +0200

    changed the layout of the header and added personal picture

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h4>Hello I'm</h4>
         <h1>Moritz Künzl</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>a future developer</h5>
         <CTA />
         <HeaderSocial/>
 
-        <div className='personalPicture'>
+        {/*<div className='personalPicture'>
           <img src={PersonalPicture} alt="Myself" />
-        </div>
+  </div>*/}
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
