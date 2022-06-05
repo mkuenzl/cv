240b9cccff152bfb996b8b9c6efad52a0416d86a
Moritz Künzl <mo.kuenzl@gmail.com>
2022-06-05 13:59:18 +0200

    cleaned about section and fixed media queries for phones

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 import './about.css'
-import Card from './Card'
-import PersonalPicture from '../../assets/pictures/me-about.jpg'
+
+// Import Icons
 import {GiPlantRoots} from 'react-icons/gi'
 import {GiCampCookingPot} from 'react-icons/gi'
 import {GiMountainClimbing} from 'react-icons/gi'
 import {BiCodeCurly} from 'react-icons/bi'
-import {SiPcgamingwiki} from 'react-icons/si'
+
+
 
 const About = () => {
 
@@ -30,38 +31,6 @@ const About = () => {
           </h2>
         </div>
         <div className="about__content">
-          {/*<div className="about__cards">
-            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Plants")} onMouseLeave={() => setAboutText(standardText)}>
-              <GiPlantRoots className='about__icon'/>
-              <h5>Plants</h5>
-            </article>
-            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Cooking")} onMouseLeave={() => setAboutText(standardText)}>
-              <GiCampCookingPot className='about__icon'/>
-              <h5>Food</h5>
-            </article>
-            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Bouldering")} onMouseLeave={() => setAboutText(standardText)}>
-              <GiMountainClimbing className='about__icon'/>
-              <h5>Boulder</h5>
-            </article>
-	          <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
-              <BiCodeCurly className='about__icon'/>
-              <h5>Code</h5>
-            </article>
-            <article className='about__card' onMouseEnter={() => setAboutText("Changed Text Coding")} onMouseLeave={() => setAboutText(standardText)}>
-              <SiPcgamingwiki className='about__icon'/>
-              <h5>Games</h5>
-            </article>
-          </div>*/}
-
-          {/*<div className="about__me">
-            <div className="about__me-image">
-              <img src={PersonalPicture} alt="About picture of myself." />
-            </div>
-            <div className="about__me-text">
-              <p>{aboutText}</p>
-            </div>
-          </div>*/}
-
           <div className="about__card-row">
             <div className="about__card">
               <GiPlantRoots className='about__icon-set'/>
@@ -81,9 +50,6 @@ const About = () => {
             <h2>Box 1</h2>
             <p>Lorem ipsum..</p>
           </div>
-          
-          
-
         </div>
       </div>
     </section>
